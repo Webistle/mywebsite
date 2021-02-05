ae612ffb4ed3d378cd1ebd0f1e977ae5164993f5
Webistle <76656814+Webistle@users.noreply.github.com>
2021-02-06 01:03:08 +0530

    b

--- newun/assets/js/main.js
+++ newun/assets/js/main.js
@@ -6,7 +6,7 @@
 * Version  : 2.0
 * Author   : Codings
 * Support  : codings.dev
-* 
+*
 ----------------------------------------------*/
 
 /*----------------------------------------------
@@ -15,13 +15,13 @@
 
 1. Preloader
 2. Responsive Menu
-3. Navigation 
-4. Slides 
+3. Navigation
+4. Slides
 5. Particles
 6. Progress Bar
 7. Shuffle
 8. Sign and Register Form
-9. Multi-Step Form 
+9. Multi-Step Form
 10. Simple Form
 11. Recaptcha
 12. Cookie Notice
@@ -973,7 +973,7 @@ jQuery(function($) {
             'leverage_form_wpnonce': wpnonce
         };
 
-        $.valid_email = false;
+        $.valid_email = true;
 
         timer = setTimeout(function() {
 
@@ -1230,14 +1230,7 @@ jQuery(function($) {
 
         var url = leverage_form.attr('action');
 
-        $.ajax({
-            type: 'POST',
-            url: url,
-            data: leverage_form.serialize(),
-            success: function() {
-                submissionDone();
-            }
-        })
+
     })
 })
 
@@ -1331,73 +1324,4 @@ jQuery(function($) {
 
     sendForm('#leverage-simple-form');
     sendForm('#leverage-subscribe');
-})
-
-/*----------------------------------------------
-11. Recaptcha
-----------------------------------------------*/
-
-jQuery(function($) {
-
-    'use strict';
-
-    if ($('input[name="reCAPTCHA"]').length) {
-
-        let siteKey = "6Lf-NwEVAAAAAPo_wwOYxFW18D9_EKvwxJxeyUx7"; // Put your site key here
-
-        if (siteKey) {
-            $('input[name="reCAPTCHA"]').attr("data-key", siteKey);
-            grecaptcha.ready(function() {
-                grecaptcha.execute(siteKey, {
-                    action: "create_comment"
-                }).then(function(token) {
-                    $('input[name="reCAPTCHA"]').val(token);
-                })
-            })
-        }
-    }
-})
-
-/*----------------------------------------------
-12. Cookie Notice
-----------------------------------------------*/
-
-jQuery(function($) {
-
-    'use strict';
-
-    let cookieNotice = true;
-
-    if (cookieNotice) {
-
-        // Translate
-        gdprCookieNoticeLocales.en = {
-            description: 'We use cookies to offer you a better browsing experience, personalise content and ads, to provide social media features and to analyse our traffic. Read about how we use cookies and how you can control them by clicking Cookie Settings. You consent to our cookies if you continue to use this website.',
-            settings: 'Cookie settings',
-            accept: 'Accept cookies',
-            statement: 'Our cookie statement',
-            save: 'Save settings',
-            always_on: 'Always on',
-            cookie_essential_title: 'Essential website cookies',
-            cookie_essential_desc: 'Necessary cookies help make a website usable by enabling basic functions like page navigation and access to secure areas of the website. The website cannot function properly without these cookies.',
-            cookie_performance_title: 'Performance cookies',
-            cookie_performance_desc: 'These cookies are used to enhance the performance and functionality of our websites but are non-essential to their use. For example it stores your preferred language or the region that you are in.',
-            cookie_analytics_title: 'Analytics cookies',
-            cookie_analytics_desc: 'We use analytics cookies to help us measure how users interact with website content, which helps us customize our websites and application for you in order to enhance your experience.',
-            cookie_marketing_title: 'Marketing cookies',
-            cookie_marketing_desc: 'These cookies are used to make advertising messages more relevant to you and your interests. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.'
-        }
-
-        gdprCookieNotice({
-            locale: 'en', // This is the default value
-            timeout: 2000, // Time until the cookie bar appears
-            expiration: 30, // This is the default value, in days
-            domain: window.location.hostname, // If you run the same cookie notice on all subdomains, define the main domain starting with a .
-            implicit: true, // Accept cookies on page scroll automatically
-            statement: 'https://leverage.codings.dev', // Link to your cookie statement page
-            performance: ['JSESSIONID'], // Cookies in the performance category.
-            analytics: ['ga'], // Cookies in the analytics category.
-            marketing: ['SSID'] // Cookies in the marketing category.
-        })
-    }
 })
